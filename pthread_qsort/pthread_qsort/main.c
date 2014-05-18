2dcc464f35e674ee60ea68ba0572a8f66dcfd009
purepennons <y80811@gmail.com>
2014-05-18 18:11:38 +0800

    add timer function.

--- pthread_qsort/pthread_qsort/main.c
+++ pthread_qsort/pthread_qsort/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
 #include "Struct.h"
 #include "FileAPI.h"
@@ -15,6 +16,8 @@ int main(int argc, char *argv[]) {
     if (argc !=4 || atoi(argv[1])%2!=0) {
         printf("錯誤的參數格式.\n");
         printf("\n");
+        printf("argc = 4\n");
+        printf("\n");
         printf("argv[0] = 執行檔, argv[1] = thread 數量, argv[2] = input檔名, argv[3] = output檔名\n");
         printf("\n");
         printf("thread數量需為2的倍數.\n");
@@ -23,9 +26,23 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
     
+    //time record parameters
+    clock_t start, end;
+    
+    //start to record.
+    start = clock();
+    
     //qsort main function
     struct InputAttribute inputStruct = getInputAttribute(argv[2], atoi(argv[1]));
     struct outputAttribute outputStruct =  pthread_qsort((void *)&inputStruct);
     writeFileByOutputAttribute(argv[3], (void *)&outputStruct);
+    
+    //end to record.
+    end = clock();
+    
+    double diff = end - start; // ms
+    printf("總耗時： %f  sec\n", diff / CLOCKS_PER_SEC );
+
+    
     pthread_exit(NULL);
 }
