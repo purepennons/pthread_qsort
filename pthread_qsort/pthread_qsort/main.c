288880315a1fae140a8b3a86577dc61cfdeb9bb8
purepennons <y80811@gmail.com>
2014-05-18 17:57:31 +0800

    新增程式執行時的參數讀取。

--- pthread_qsort/pthread_qsort/main.c
+++ pthread_qsort/pthread_qsort/main.c
@@ -6,12 +6,26 @@
 #include "OtherAPI.h"
 #include "pthread_qsort.h"
 
-#define INPUT_FILE "input.txt"
-#define NUM_OF_THREADS 8
+//#define INPUT_FILE "input.txt"
+//#define NUM_OF_THREADS 8
+
+//argv[0] = 執行檔, argv[1] = thread 數量, argv[2] = input檔名, argv[3] = output檔名
 
 int main(int argc, char *argv[]) {
-    struct InputAttribute inputStruct = getInputAttribute(INPUT_FILE, NUM_OF_THREADS);
+    if (argc !=4 || atoi(argv[1])%2!=0) {
+        printf("錯誤的參數格式.\n");
+        printf("\n");
+        printf("argv[0] = 執行檔, argv[1] = thread 數量, argv[2] = input檔名, argv[3] = output檔名\n");
+        printf("\n");
+        printf("thread數量需為2的倍數.\n");
+        printf("\n");
+        printf("EX: ./exe 4 input.txt output.txt\n");
+        exit(-1);
+    }
+    
+    //qsort main function
+    struct InputAttribute inputStruct = getInputAttribute(argv[2], atoi(argv[1]));
     struct outputAttribute outputStruct =  pthread_qsort((void *)&inputStruct);
-    int flag =  writeFileByOutputAttribute("sort.txt", (void *)&outputStruct);
+    writeFileByOutputAttribute(argv[3], (void *)&outputStruct);
     pthread_exit(NULL);
 }
